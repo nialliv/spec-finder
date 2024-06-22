e7121c7523aabc60560068513630fb43cc43e838
nialliv <artemjevnikita@yandex.ru>
2024-06-22 12:19:20 +0400

    refactor

--- src/main/java/ru/aprtemev/specfinder/controller/PrinterController.java
+++ src/main/java/ru/aprtemev/specfinder/controller/PrinterController.java
@@ -1,14 +1,22 @@
 package ru.aprtemev.specfinder.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import ru.aprtemev.specfinder.dto.PrinterRequestDto;
 import ru.aprtemev.specfinder.dto.PrinterResponseDto;
 import ru.aprtemev.specfinder.service.PrinterService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/printers")
